7da24cb3f08ad08b5b3ceb3ff95fe3e4babeeb71
SpeedoDevo <speedo@devo.hu>
2014-03-20 20:47:11 +0000

    animating objec tselection done

--- public/js/controllers/CreateCtrl.js
+++ public/js/controllers/CreateCtrl.js
@@ -4,6 +4,11 @@ angular.module('CreateCtrl', []).controller('CreateController', function($scope)
 	$scope.imgSrc="";
 	$scope.shape.type = "";
 	$scope.shape.bg = "#ffffff";
+	$scope.shape.rw = $scope.shape.dom.width();
+	$scope.shape.rh = $scope.shape.dom.height();
+	$scope.shape.cw = $scope.shape.dom.width();
+	$scope.shape.ch = $scope.shape.dom.height();
+
 
 	$scope.$on('$routeChangeSuccess', function () {
 		center();
@@ -22,19 +27,26 @@ angular.module('CreateCtrl', []).controller('CreateController', function($scope)
 		});
 	});
 	$scope.$watch('shape.type',function (newVal,oldVal) {
-		console.log(newVal);
 		if (newVal === "circle") {
+			console.log(newVal);
 			$scope.shape.rw = $scope.shape.dom.width();
 			$scope.shape.rh = $scope.shape.dom.height();
 			$scope.shape.dom.animate({
-				"height": "400px",
-				"width": "400px",
+				"width": $scope.shape.cw + "px",
+				"height": $scope.shape.ch + "px",
 				"border-radius": "50%"},
 				{step: function (now, tween) {center();}},
 				1000);
 		} else{
-			$scope.shape.rw = $scope.shape.dom.width();
-			$scope.shape.rh = $scope.shape.dom.height();
+			console.log(newVal);
+			$scope.shape.cw = $scope.shape.dom.width();
+			$scope.shape.ch = $scope.shape.dom.height();
+			$scope.shape.dom.animate({
+				"width": $scope.shape.rw + "px",
+				"height": $scope.shape.rh + "px",
+				"border-radius": "0%"},
+				{step: function (now, tween) {center();}},
+				1000);
 		};
 	})
 	$(window).resize(function(){
