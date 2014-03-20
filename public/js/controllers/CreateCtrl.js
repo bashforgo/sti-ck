afee53e11179287a2dfbbf4dffc7f3be34b9f667
SpeedoDevo <speedo@devo.hu>
2014-03-20 14:57:04 +0000

    grouped buttons properly

--- public/js/controllers/CreateCtrl.js
+++ public/js/controllers/CreateCtrl.js
@@ -1,5 +1,7 @@
 angular.module('CreateCtrl', []).controller('CreateController', function($scope) {
 
 	$scope.tagline = 'Create something new!';
-
+	$scope.$on('$routeChangeSuccess', function () {
+	  // run some code to do your animations
+	});
 });
